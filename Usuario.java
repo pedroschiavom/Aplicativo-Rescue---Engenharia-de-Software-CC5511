ca39bfb5dc21230d970d3e92baf4cbcfba4bfb8e
pedroschiavom <31569709+pedroschiavom@users.noreply.github.com>
2018-11-29 16:51:29 -0200

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,104 @@
+package pedro.br.myapplication.modelo;
+
+
+public class Usuario {
+    private String nome;
+    private String idade;
+    private String endereco;
+    private String telefone;
+    private String sus;
+    private String convenio;
+    private String contato1;
+    private String contato2;
+    private String sangue;
+    private String alergia;
+    private String medicamento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSus() {
+        return sus;
+    }
+
+    public void setSus(String sus) {
+        this.sus = sus;
+    }
+
+    public String getConvenio() {
+        return convenio;
+    }
+
+    public void setConvenio(String convenio) {
+        this.convenio = convenio;
+    }
+
+    public String getContato1() {
+        return contato1;
+    }
+
+    public void setContato1(String contato1) {
+        this.contato1 = contato1;
+    }
+
+    public String getContato2() {
+        return contato2;
+    }
+
+    public void setContato2(String contato2) {
+        this.contato2 = contato2;
+    }
+
+    public String getSangue() {
+        return sangue;
+    }
+
+    public void setSangue(String sangue) {
+        this.sangue = sangue;
+    }
+
+    public String getAlergia() {
+        return alergia;
+    }
+
+    public void setAlergia(String alergia) {
+        this.alergia = alergia;
+    }
+
+    public String getMedicamento() {
+        return medicamento;
+    }
+
+    public void setMedicamento(String medicamento) {
+        this.medicamento = medicamento;
+    }
+}
